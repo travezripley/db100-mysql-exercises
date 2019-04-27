669438537f89ceffced956023d5de2770f4b4b20
Travis Ripley <theripleys@gmail.com>
2019-04-27 15:24:53 -0700

    exercise.sql - completed

--- sql/exercises.sql
+++ sql/exercises.sql
@@ -13,10 +13,16 @@
 ## 1. SELECT statements
 
 # 1a. Select all columns from the actor table.
-SELECT * FROM actor;
+SELECT 
+    *
+FROM
+    actor;
 
 # 1b. Select only the last_name column from the actor table.
-SELECT last_name FROM actor;
+SELECT 
+    last_name
+FROM
+    actor;
 
 # 1c. Select only the following columns from the film table.
 #
@@ -26,13 +32,14 @@ SELECT last_name FROM actor;
 # rental_duration       Exists in film table.
 # rental_rate           Exists in film table.
 # total_rental_cost     rental_duration * rental_rate
-SELECT
-    title, 
-    description, 
-    rental_duration, 
-    rental_rate, 
-    rental_duration * rental_rate as total_rental_cost
-FROM film;
+SELECT 
+    title,
+    description,
+    rental_duration,
+    rental_rate,
+    rental_duration * rental_rate AS total_rental_cost
+FROM
+    film;
 
 # ---------------------------------------------------------#
 
@@ -40,15 +47,24 @@ FROM film;
 ## 2. DISTINCT operator
 
 # 2a. Select all distinct (different) last names from the actor table.
-SELECT DISTINCT last_name FROM actor;
+SELECT DISTINCT
+    last_name
+FROM
+    actor;
 
 
 # 2b. Select all distinct (different) postal codes from the address table.
-SELECT DISTINCT postal_code FROM address;
+SELECT DISTINCT
+    postal_code
+FROM
+    address;
 
 
 # 2c. Select all distinct (different) ratings from the film table.
-SELECT DISTINCT rating FROM film;
+SELECT DISTINCT
+    rating
+FROM
+    film;
 
 
 # ---------------------------------------------------------#
@@ -57,55 +73,97 @@ SELECT DISTINCT rating FROM film;
 ## 3. WHERE clause
 
 # 3a. Select the title, description, rating, movie length columns from the films table that last 3 hours or longer.
-SELECT title,description,rating,length FROM film WHERE length>=180;
+SELECT 
+    title, description, rating, length
+FROM
+    film
+WHERE
+    length >= 180;
 
 # 3b. Select the payment id, amount, and payment date columns from the payments table for payments made on or after 05/27/2005.
-SELECT payment_id, amount, payment_date FROM payment WHERE payment_date >=DATE('2005-05-27 00:00:00');
+SELECT 
+    payment_id, amount, payment_date
+FROM
+    payment
+WHERE
+    payment_date >= DATE('2005-05-27 00:00:00');
 
 
 # 3c. Select the primary key, amount, and payment date columns from the payment table for payments made on 05/27/2005.
-SELECT payment_id, amount, payment_date FROM payment WHERE payment_date >= DATE('2005-05-27 00:00:00') AND payment_date <= DATE('2005-05-28 00:00:00');
+SSELECT 
+    payment_id, amount, payment_date
+FROM
+    payment
+WHERE
+    payment_date >= DATE('2005-05-27 00:00:00')
+        AND payment_date <= DATE('2005-05-28 00:00:00');
 
 
 # 3d. Select all columns from the customer table for rows that have a last name beginning with "S" and a first name ending with "N".
-SELECT * FROM customer WHERE last_name LIKE '%s' AND first_name LIKE 'n%';
+SELECT 
+    *
+FROM
+    customer
+WHERE
+    last_name LIKE '%s'
+        AND first_name LIKE 'n%';
 
 
 # 3e. Select all columns from the customer table for rows where the customer is inactive or has a last name ending with "M".
-SELECT * FROM customer WHERE active=0 or last_name LIKE 'm%';
+SELECT 
+    *
+FROM
+    customer
+WHERE
+    active = 0 OR last_name LIKE 'm%';
 
 
 # 3f. Select all columns from the category table for rows where the primary key is greater than 4 and the name field begins with either "C", "S" or "T".
-SELECT * FROM category WHERE category_id < 4 AND (name LIKE 'n%') or (name LIKE 'c%') or (name LIKE 't%');
+SELECT 
+    *
+FROM
+    category
+WHERE
+    category_id < 4 AND (name LIKE 'n%')
+        OR (name LIKE 'c%')
+        OR (name LIKE 't%');
 
 # 3g. Select all columns minus the password column from the staff table for rows that contain a password.
 SELECT 
-    staff_id, 
-    first_name, 
-    last_name, 
-    address_id, 
-    picture, email, 
-    store_id, 
-    active, 
-    username, 
-    password, 
-    last_update 
-    FROM staff WHERE password IS NOT NULL;
+    staff_id,
+    first_name,
+    last_name,
+    address_id,
+    picture,
+    email,
+    store_id,
+    active,
+    username,
+    password,
+    last_update
+FROM
+    staff
+WHERE
+    password IS NOT NULL;
 
 
 # 3h. Select all columns minus the password column from the staff table for rows that do not contain a password.
 SELECT 
-    staff_id, 
-    first_name, 
-    last_name, 
-    address_id, 
-    picture, email, 
-    store_id, 
-    active, 
-    username, 
-    password, 
-    last_update 
-    FROM staff WHERE password IS NULL;
+    staff_id,
+    first_name,
+    last_name,
+    address_id,
+    picture,
+    email,
+    store_id,
+    active,
+    username,
+    password,
+    last_update
+FROM
+    staff
+WHERE
+    password IS NULL;
 
 # ---------------------------------------------------------#
 
@@ -113,13 +171,12 @@ SELECT
 ## 4. IN operator
 
 # 4a. Select the phone and district columns from the address table for addresses in California, England, Taipei, or West Java.
-SELECT
-	phone,
-    district
-    FROM address
-    WHERE district
-    IN 
-    ('California', 'England', 'Taipei', 'West Java');
+SELECT 
+    phone, district
+FROM
+    address
+WHERE
+    district IN ('California' , 'England', 'Taipei', 'West Java');
 
 
 # 4b. Select the payment id, amount, and payment date columns from the payment table for payments made on 05/25/2005, 05/27/2005, and 05/29/2005.
@@ -301,19 +358,77 @@ LIMIT 10;
 # Label customer first_name/last_name columns as customer_first_name/customer_last_name
 # Label actor first_name/last_name columns in a similar fashion.
 
+SELECT 
+    customer.first_name AS customer_first_name,
+    customer.last_name AS customer_last_name,
+    actor.first_name AS actor_first_name,
+    actor.last_name AS actor_last_name
+FROM
+    customer
+        LEFT JOIN
+    actor ON customer.last_name = actor.last_name;
 
 # 9b. Select the customer first_name/last_name and actor first_name/last_name columns from performing a right join between the customer and actor column on the last_name column in each table. (i.e. `customer.last_name = actor.last_name`)
 
+SELECT 
+    customer.first_name AS customer_first_name,
+    customer.last_name AS customer_last_name,
+    actor.first_name AS actor_first_name,
+    actor.last_name AS actor_last_name
+FROM
+    customer
+        RIGHT JOIN
+    actor ON customer.last_name = actor.last_name;
 
 # 9c. Select the customer first_name/last_name and actor first_name/last_name columns from performing an inner join between the customer and actor column on the last_name column in each table. (i.e. `customer.last_name = actor.last_name`)
 
+SELECT 
+    customer.first_name AS customer_first_name,
+    customer.last_name AS customer_last_name,
+    actor.first_name AS actor_first_name,
+    actor.last_name AS actor_last_name
+FROM
+    customer
+        INNER JOIN
+    actor ON customer.last_name = actor.last_name;
 
 # 9d. Select the city name and country name columns from the city table, performing a left join with the country table to get the country name column.
+
+SELECT 
+    city AS city, country AS country
+FROM
+    city
+        LEFT JOIN
+    country ON city.country_id = country.country_id;
+    
+    
 
 
 # 9e. Select the title, description, release year, and language name columns from the film table, performing a left join with the language table to get the "language" column.
 # Label the language.name column as "language" (e.g. `select language.name as language`)
 
+SELECT 
+    title, description, release_year, language.name AS language
+FROM
+    film
+        LEFT JOIN
+    language ON film.language_id = language.language_id;
+
 
 # 9f. Select the first_name, last_name, address, address2, city name, district, and postal code columns from the staff table, performing 2 left joins with the address table then the city table to get the address and city related columns.
 
+SELECT 
+    first_name,
+    last_name,
+    address.address AS address,
+    address.address2 AS address2,
+    city.city AS city,
+    address.district AS district,
+    address.postal_code AS postal_code
+FROM
+    staff
+        LEFT JOIN
+    address ON staff.address_id = address.address_id
+        LEFT JOIN
+    city ON address.city_id = city.city_id;
+  
